1311424e46bd8b97db2c4dc1573e32b0722df99a
Gergő Fándly <gergo@paymoapp.com>
2026-04-06 12:48:30 +0300

    fix(logger): Changed timestamp format

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,14 @@
 import winston from 'winston';
 
+const timestamp = winston.format(info => {
+	info.time = Date.now();
+
+	return info;
+});
+
 const logger = winston.createLogger({
 	level: 'info',
-	format: winston.format.combine(
-		winston.format.timestamp(),
-		winston.format.json()
-	),
+	format: winston.format.combine(timestamp(), winston.format.json()),
 	defaultMeta: {
 		service: 'twitch-valorant-rr-bot'
 	},
